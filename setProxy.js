cd0cb6c9d225f096b354414a24134bcaea1dcbfc
MinYoungChoe <mchoe@myseneca.ca>
2024-06-02 13:06:28 -0400

    Undo the editing

--- setProxy.js
+++ setProxy.js
@@ -1,21 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://localhost:8080',	
-//       changeOrigin: true,
-//     })
-//   );
-// };
-module.exports = {
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "https://libertyrunners-app.com",
-        changeOrigin: true,
-      },
-    },
-  },
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',	
+      changeOrigin: true,
+    })
+  );
 };
